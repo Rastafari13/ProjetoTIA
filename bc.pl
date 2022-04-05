686e8f10b26534a10a92bbb4ca3b24373af29c68
Rastafari13 <mscancela2001@hotmail.com>
2022-04-05 16:30:13 +0100

    trabalho 5 de abril

---
+++ bc.pl
@@ -0,0 +1,33 @@
+%base de conhecimentos
+
+%criar perfis
+alojamento(X,W,Z):- findall(Y,(filme(Y,H,_,U,_,_,_,W), H>=Z, membro(X,U)),K), print(K).
+
+if relaxar and pa and ginasio and restaurante then 1.
+if relaxar and pa and ginasio and nao_restaurante then 2.
+if relaxar and pa and nao_ginasio and nao_restaurante then 3.
+if relaxar and nao_pa and nao_ginasio and nao_restaurante then 4.
+if relaxar and nao_pa and nao_ginasio and restaurante then 5.
+if relaxar and nao_pa and ginasio and restaurante then 6.
+if relaxar and nao_pa and ginasio and nao_restaurante then 7.
+if relaxar and pa and nao_ginasio and restaurante then 8.
+
+%conhecer a ilha
+if ilha and carro and pa and ginasio and restaurante then 9.
+if ilha and carro and nao_pa and nao_ginasio and nao_restaurante then 10.
+if ilha and carro and nao_pa and nao_ginasio and restaurante then 11.
+if ilha and carro and nao_pa and ginasio and restaurante then 12.
+if ilha and carro and nao_pa and ginasio and nao_restaurante then 13.
+if ilha and carro and pa and nao_ginasio and nao_restaurante then 14.
+if ilha and carro and pa and nao_ginasio and restaurante then 15.
+if ilha and carro and pa and ginasio and nao_restaurante then 16.
+if ilha and nao_carro and nao_pa and nao_ginasio and nao_restaurante then 17.
+if ilha and nao_carro and nao_pa and nao_ginasio and restaurante then 18.
+if ilha and nao_carro and nao_pa and ginasio and restaurante then 19.
+if ilha and nao_carro and nao_pa and ginasio and nao_restaurante then 20.
+if ilha and nao_carro and pa and nao_ginasio and nao_restaurante then 21.
+if ilha and nao_carro and pa and nao_ginasio and restaurante then 22.
+if ilha and nao_carro and pa and ginasio and nao_restaurante then 23.
+if ilha and nao_carro and pa and ginasio and restaurante then 24.
+
+
