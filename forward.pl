234d0de52c180a5f18574eb2e013821763696814
Rastafari13 <mscancela2001@hotmail.com>
2022-04-13 16:57:57 +0100

    13 abril

--- forward.pl
+++ forward.pl
@@ -5,26 +5,30 @@
 :- op( 300, xfy, or).
 :- op( 500, xfy, and).
 
-result:- new_derived_fact(P),             
+result:- new_derived_fact(P),
    !,
    resultadowrite(P), nl,
    assert(fact(P)),
-   result.                           
+   result.
 result:- nl,write('Esperemos que goste das nossas recomendacoes, tenha uma excelente estadia!') .
 
 new_derived_fact( Concl)  :-
-   if Cond then Concl,               
-   \+ fact( Concl),                 
-   composed_fact( Cond).             
+   if Cond then Concl,
+   \+ fact( Concl),
+   composed_fact( Cond).
 
 composed_fact( Cond)  :-
-   fact( Cond).                      
+   fact( Cond).
 
 composed_fact( Cond1 and Cond2)  :-
    composed_fact( Cond1),
-   composed_fact( Cond2).            
+   composed_fact( Cond2).
 
 composed_fact( Cond1 or Cond2)  :-
    composed_fact( Cond1)
    ;
    composed_fact( Cond2).
+
+
+
+
